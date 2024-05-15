8a76897ebd94e7cb7d3604d65d44b46209614a4b
Jia XU <xujia118@hotmail.com>
2024-05-14 22:29:45 -0700

    Made updateCart function

--- backend/models/cart.ts
+++ backend/models/cart.ts
@@ -1,5 +1,8 @@
-import User from "../schemas/User";
 import mongoose from "mongoose";
+const { ObjectId } = mongoose.Types;
+
+import User from "../schemas/User";
+import Dish from "../schemas/Dish";
 
 // async function getCartFromUser(userId: string) {
 //   try {
@@ -22,63 +25,64 @@ import mongoose from "mongoose";
 // }
 
 async function updateCart(userId: string, dishId: string, operator: string) {
-  const { ObjectId } = mongoose.Types;
-
-  const userIdObj = new ObjectId(userId);
-  const dishIdObj = new ObjectId(dishId);
-
-  // now cart is a mongoDB document embedding dish object, we can easily find and update
   try {
-    // const cart = await User.findOne({ _id: userIdObj, "cart.dish": dishIdObj });
-    const user = await User.findOne({ _id: new ObjectId(userId) });
-    if (!user ||!user.cart) {
-      throw new Error("User or dish not found in the cart.");
+    // Get user
+    const user = await User.findById(userId);
+    if (!user || !user.cart) {
+      throw new Error("User or cart not found");
     }
 
-    const cartItem = user.cart.find((item) => item.dish._id.equals(dishIdObj));
-    
-    if (!cartItem) {
-      throw new Error("Dish not found in the cart.");
-    }
-    
-    const price = cartItem.dish.price;
-    let quantity = cartItem.quantity;
-    let subtotal = cartItem.subtotal;
-
-    if (operator === "+") {
-      quantity += 1;
-      subtotal += price;
-    } else {
-      quantity -= 1;
-      subtotal -= price;
+    // Get dish
+    const dish = await Dish.findById(dishId);
+    if (!dish) {
+      throw new Error("Dish not found");
     }
 
-    await User.updateOne(
-      { _id: userIdObj, "cart.dish": dishIdObj },
-      {
-        $inc: {
-          "cart.$.quantity": quantity,
-          "cart.$.subtotal": subtotal,
-        },
+    // Get dish price, since we need it in cartItem
+    const price = dish.price;
+    for (const cartItem of user.cart) {
+      if ((cartItem.dish as any)._id.toString() === dishId) {
+        
+        // Update quantity and subtotal;
+        let quantity = cartItem.quantity;
+        if (operator === "+") {
+          quantity++;
+        } else if (operator === "-" && quantity > 0) {
+          quantity--;
+        }
+        const subtotal = price * quantity;
+ 
+        cartItem.quantity = quantity;
+        cartItem.subtotal = subtotal;
+
+        // Save the updated user document
+        await user.save();
+        return;
       }
-    );
+    }
+ 
+    // push
+    const quantity = 1;
+    const subtotal = price * quantity;
+    user.cart.push({ dish, quantity, subtotal });
+    await user.save();
+    return;
   } catch (err) {
     console.log(err);
     throw err;
   }
 }
 
-const userId1 = "663d6e6b7643576b26aa5497"; // Bob
-const dishId1 = "663d80b7f6597992c77971d0";
-const operator = "+";
+// const userId1 = "663d6e6b7643576b26aa5497"; // Bob
+// const dishId1 = "663d80b7f6597992c77971d3"; // First dish
 
 const userId2 = "663d6e6c7643576b26aa5499"; // Alice
 const dishId2 = "663d80b7f6597992c77971d3";
-// const operator = "+"
+
+const operator = "-";
 
 export function runCartTest() {
-  updateCart(userId1, dishId1, operator);
-  // updateCart(userId2, dishId2, operator);
+  updateCart(userId2, dishId2, operator);
 }
 
 // function calculateTotal(userCart: Cart) {
