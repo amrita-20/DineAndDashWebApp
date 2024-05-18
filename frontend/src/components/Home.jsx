f8c331aee0c7fbfc34cdef71ed064fff41615b0c
Jia XU <xujia118@hotmail.com>
2024-05-17 22:07:02 -0700

    Cleared up in Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,26 +2,26 @@ import { useEffect } from "react";
 import { fetchMenu } from "../services";
 import NavBar from "./NavBar";
 
-function Home () {
-      function onFetchMenu() {
-        fetchMenu()
-          .then((data) => {
-            console.log(data.menu);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
+function Home() {
+  function onFetchMenu() {
+    fetchMenu()
+      .then((data) => {
+        console.log(data.menu);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-      useEffect(() => {
-        onFetchMenu();
-      }, []);
-    return (
-      <div>
-        <NavBar />
-        <button className="" onClick={() => onFetchMenu()}>Get menu</button>
-      </div>
-    );
+  useEffect(() => {
+    onFetchMenu();
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
 export default Home;
