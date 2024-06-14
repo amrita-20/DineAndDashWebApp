57879b1c3325c20399432d7639f4aa62cec2183b
Jia XU <xujia118@hotmail.com>
2024-06-14 08:54:30 -0700

    Updated dropmenu with MUI

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,16 +3,32 @@ import { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { AccountCircle } from "@mui/icons-material";
+
+// 以下icons全不要
 import HomeIcon from "../assets/home_FILL0_wght400_GRAD0_opsz24.svg";
 import SearchIcon from "../assets/search_FILL0_wght400_GRAD0_opsz24.svg";
 import AccountIcon from "../assets/account_circle_FILL0_wght400_GRAD0_opsz24.svg";
 import CartIcon from "../assets/shopping_cart_FILL0_wght400_GRAD0_opsz24.svg";
 
 import "../css/NavBar.css";
+import { IconButton } from "@mui/material";
 
 function NavBar({ cartItems, filterMenu, filteredMenu, setFilteredMenu }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const [showMenu, setShowMenu] = useState(false);
-  const [filter, setFilter] = useState("")
+  const [filter, setFilter] = useState("");
 
   function handleSubmitSearch(e) {
     e.preventDefault();
@@ -54,39 +70,65 @@ function NavBar({ cartItems, filterMenu, filteredMenu, setFilteredMenu }) {
           <span className="cart-count">{cartItems.length}</span>
         </button>
         {isAuthenticated ? (
-          <nav className="dropmenu" onMouseLeave={() => setShowMenu(false)}>
-            <button
-              className="button-dropmenu"
-              onClick={() => setShowMenu(!showMenu)}
+          <nav>
+            <IconButton
+              color="inherit"
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
             >
-              <img
-                className="account-icon"
-                src={AccountIcon}
-                alt="control-center"
-              />
-              {/* <span>{user?.email}</span> */}
-            </button>
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem component={Link} to="/menu" onClick={handleClose}>
+                Menu
+              </MenuItem>
 
-            <ul className={`menu-list ${showMenu ? "show" : ""}`}>
-              <li className="menu-item">
-                <Link to="/menu">Menu</Link>
-              </li>
-              <li className="menu-item">
-                <Link to="/user-profile">Profile</Link>
-              </li>
               {user.name === "Amrita Dubey" ? (
-                <li className="menu-item">
-                  <Link to="/order-management">Orders Management</Link>
-                </li>
+                <MenuItem
+                  component={Link}
+                  to="/order-status"
+                  onClick={handleClose}
+                >
+                  Orders Status
+                </MenuItem>
               ) : (
-                <li className="menu-item">
-                  <Link to="/order-status">Orders Status</Link>
-                </li>
+                <MenuItem
+                  component={Link}
+                  to="/order-management"
+                  onClick={handleClose}
+                >
+                  Orders Management
+                </MenuItem>
               )}
-              <li className="menu-item">
-                <Link onClick={() => logout()}>Logout</Link>
-              </li>
-            </ul>
+
+              <MenuItem
+                component={Link}
+                to="/user-profile"
+                onClick={handleClose}
+              >
+                My account
+              </MenuItem>
+              <MenuItem
+                component={Link}
+                onClick={() => {
+                  handleClose();
+                  logout();
+                }}
+              >
+                Logout
+              </MenuItem>
+            </Menu>
           </nav>
         ) : (
           <button onClick={async () => await loginWithRedirect()}>Login</button>
