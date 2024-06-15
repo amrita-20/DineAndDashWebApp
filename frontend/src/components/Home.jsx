09a6ea65b48e8b4875e1dfaf3a8f6a8c9f74d035
Jia XU <xujia118@hotmail.com>
2024-06-15 15:46:31 -0700

    Added a simple homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,28 @@
-
-import NavBar from "./NavBar";
-// import "../css/Home.css";
+import "../css/Home.css";
 
 function Home() {
   return (
     <>
-      Home
+      <div className="home-image-container">
+        <img
+          className="home-image"
+          src="/images/vit-ch-Oxb84ENcFfU-unsplash.jpg"
+          alt="Pizza"
+        />
+        <h1 className="home-title">La Vita Bella</h1>
+      </div>
+      <p className="home-text">
+        Welcome to Bonnati, where the spirit of Italy comes alive in our
+        Summer Festival of Pizzas! This season, indulge in a vibrant celebration
+        of flavors with our artisanal pizzas, crafted to perfection by our
+        master pizzaiolos. Savor the sunshine with our special creations like
+        the Caprese Delight, featuring sun-ripened tomatoes, creamy buffalo
+        mozzarella, and fragrant basil, or the Mediterranean Medley, bursting
+        with roasted vegetables, olives, and tangy feta cheese. Join us in our
+        festive outdoor garden, adorned with twinkling lights and the lively
+        sounds of traditional Italian music, making every bite an unforgettable
+        summer experience. Buon appetito!
+      </p>
     </>
   );
 }
