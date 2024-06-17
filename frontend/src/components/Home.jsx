bb53486dec87b88cef6be8e654e7564ead1756d1
Jia XU <xujia118@hotmail.com>
2024-06-17 11:06:06 -0700

    Fixed CAT in homepage; cart display in mobile screen

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { IconButton } from "@mui/material";
 import "../css/Home.css";
 
 function Home() {
@@ -12,20 +13,22 @@ function Home() {
         />
         <h1 className="home-title">La Vita Bella</h1>
       </div>
-     <div className="menu-navigation">
-     <Link to="/menu" className="explore-menu">Explore Menu</Link>
-      <p className="home-text">
-        Welcome to Dine&Dash, where the spirit of Italy comes alive in our
-        Summer Festival of Pizzas! This season, indulge in a vibrant celebration
-        of flavors with our artisanal pizzas, crafted to perfection by our
-        master pizzaiolos. Savor the sunshine with our special creations like
-        the Caprese Delight, featuring sun-ripened tomatoes, creamy buffalo
-        mozzarella, and fragrant basil, or the Mediterranean Medley, bursting
-        with roasted vegetables, olives, and tangy feta cheese. Join us in our
-        festive outdoor garden, adorned with twinkling lights and the lively
-        sounds of traditional Italian music, making every bite an unforgettable
-        summer experience. Buon appetito!
-      </p>
+      <div className="menu-navigation">
+        <Link to="/menu" className="explore-menu">
+          Explore Menu
+        </Link>
+        <p className="home-text">
+          Welcome to Dine&Dash, where the spirit of Italy comes alive in our
+          Summer Festival of Pizzas! This season, indulge in a vibrant
+          celebration of flavors with our artisanal pizzas, crafted to
+          perfection by our master pizzaiolos. Savor the sunshine with our
+          special creations like the Caprese Delight, featuring sun-ripened
+          tomatoes, creamy buffalo mozzarella, and fragrant basil, or the
+          Mediterranean Medley, bursting with roasted vegetables, olives, and
+          tangy feta cheese. Join us in our festive outdoor garden, adorned with
+          twinkling lights and the lively sounds of traditional Italian music,
+          making every bite an unforgettable summer experience. Buon appetito!
+        </p>
       </div>
     </>
   );
